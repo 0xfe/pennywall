48bb6accad380b6b8a8b0c9e2de8ad7849dcdfc5
0xFE <mohit@muthanna.com>
2019-02-13 20:29:36 -0500

    Update website.

--- website/upload.sh
+++ website/upload.sh
@@ -4,7 +4,5 @@ BUCKET=gs://www.pennywall.io
 URL=${BUCKET}
 
 echo Uploading website...
-gsutil -m cp -a public-read -z html,css,js index.html index.css ${URL}
-gsutil -m cp -r -a public-read -z html,css,js assets/* ${URL}/assets
-gsutil setmeta -h "Cache-control:public,max-age=120" -r ${URL}
-gsutil web set -m index.html -e index.html ${BUCKET}
+gsutil -h "Cache-control:public,max-age=600" -m cp -a public-read -z html,css,js index.html index.css ${URL}
+gsutil -h "Cache-control:public,max-age=600" -m cp -r -a public-read -z html,css,js assets/* ${URL}/assets
