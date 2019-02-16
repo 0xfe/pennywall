b897fb44b111f85a3ab54d11b3e0a843bdac2ff0
0xFE <mohit@muthanna.com>
2019-02-16 08:33:55 -0500

    Fix upload script.

--- upload_assets.sh
+++ upload_assets.sh
@@ -5,7 +5,4 @@ BUCKET=gs://give.pennywall.io
 URL=${BUCKET}
 
 echo Uploading website...
-gsutil -m cp -r -a public-read -z html,css,js,sass,hbs themes ${URL}
-
-echo Setting cache headers...
-gsutil setmeta -h "Cache-control:public,max-age=3600" -r ${URL}/themes
+gsutil -h "Cache-control:public,max-age=3600" -m cp -r -a public-read -z html,css,js,sass,hbs themes ${URL}
